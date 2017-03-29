1abf8981ffc31a830e5eae23474f53e20a1676d0
YJC <b04902053@ntu.edu.tw>
2017-03-29 09:27:00 +0800

    fix CPU_Utilization.c bug

--- hw1/kernel/CPU_Utilization.c
+++ hw1/kernel/CPU_Utilization.c
@@ -1,6 +1,8 @@
 #include <linux/kernel.h>
 #include <linux/linkage.h>
 
+#include <linux/delay.h>
+
 #include <linux/cpumask.h>
 #include <linux/fs.h>
 #include <linux/gfp.h>
@@ -14,34 +16,21 @@
 #include <linux/time.h>
 #include <linux/irqnr.h>
 #include <asm/cputime.h>
+#include <asm/div64.h>		// do_div()
 
-#ifndef arch_irq_stat_cpu
-#define arch_irq_stat_cpu(cpu) 0
-#endif
-#ifndef arch_irq_stat
-#define arch_irq_stat() 0
-#endif
 #ifndef arch_idle_time
 #define arch_idle_time(cpu) 0
 #endif
 
-static int show_stat()
+asmlinkage long sys_CPU_Utilization(void)
 {
-	int i, j;
-	unsigned long jif;
-	cputime64_t user, nice, system, idle, iowait, irq, softirq, steal;
-	cputime64_t guest;
-	u64 sum = 0;
-	u64 sum_softirq = 0;
-	unsigned int per_softirq_sums[NR_SOFTIRQS] = {0};
-	struct timespec boottime;
-	unsigned int per_irq_sum;
+	int i;
+	cputime64_t user, nice, system, idle, iowait, irq, softirq;
+	unsigned long long total = user + nice + system + idle + iowait + irq + softirq;
+	unsigned long mod;
 
 	user = nice = system = idle = iowait =
-		irq = softirq = steal = cputime64_zero;
-	guest = cputime64_zero;
-	getboottime(&boottime);
-	jif = boottime.tv_sec;
+		irq = softirq = cputime64_zero;
 
 	for_each_possible_cpu(i) {
 		user = cputime64_add(user, kstat_cpu(i).cpustat.user);
@@ -52,89 +41,26 @@ static int show_stat()
 		iowait = cputime64_add(iowait, kstat_cpu(i).cpustat.iowait);
 		irq = cputime64_add(irq, kstat_cpu(i).cpustat.irq);
 		softirq = cputime64_add(softirq, kstat_cpu(i).cpustat.softirq);
-		steal = cputime64_add(steal, kstat_cpu(i).cpustat.steal);
-		guest = cputime64_add(guest, kstat_cpu(i).cpustat.guest);
-		for_each_irq_nr(j) {
-			sum += kstat_irqs_cpu(j, i);
-		}
-		sum += arch_irq_stat_cpu(i);
-
-		for (j = 0; j < NR_SOFTIRQS; j++) {
-			unsigned int softirq_stat = kstat_softirqs_cpu(j, i);
-
-			per_softirq_sums[j] += softirq_stat;
-			sum_softirq += softirq_stat;
-		}
-	}
-	sum += arch_irq_stat();
-
-	printk("cpu  %llu %llu %llu %llu %llu %llu %llu %llu %llu\n",
-		(unsigned long long)cputime64_to_clock_t(user),
-		(unsigned long long)cputime64_to_clock_t(nice),
-		(unsigned long long)cputime64_to_clock_t(system),
-		(unsigned long long)cputime64_to_clock_t(idle),
-		(unsigned long long)cputime64_to_clock_t(iowait),
-		(unsigned long long)cputime64_to_clock_t(irq),
-		(unsigned long long)cputime64_to_clock_t(softirq),
-		(unsigned long long)cputime64_to_clock_t(steal),
-		(unsigned long long)cputime64_to_clock_t(guest));
-	for_each_online_cpu(i) {
-
-		/* Copy values here to work around gcc-2.95.3, gcc-2.96 */
-		user = kstat_cpu(i).cpustat.user;
-		nice = kstat_cpu(i).cpustat.nice;
-		system = kstat_cpu(i).cpustat.system;
-		idle = kstat_cpu(i).cpustat.idle;
-		idle = cputime64_add(idle, arch_idle_time(i));
-		iowait = kstat_cpu(i).cpustat.iowait;
-		irq = kstat_cpu(i).cpustat.irq;
-		softirq = kstat_cpu(i).cpustat.softirq;
-		steal = kstat_cpu(i).cpustat.steal;
-		guest = kstat_cpu(i).cpustat.guest;
-		printk(			"cpu%d %llu %llu %llu %llu %llu %llu %llu %llu %llu\n",
-			i,
-			(unsigned long long)cputime64_to_clock_t(user),
-			(unsigned long long)cputime64_to_clock_t(nice),
-			(unsigned long long)cputime64_to_clock_t(system),
-			(unsigned long long)cputime64_to_clock_t(idle),
-			(unsigned long long)cputime64_to_clock_t(iowait),
-			(unsigned long long)cputime64_to_clock_t(irq),
-			(unsigned long long)cputime64_to_clock_t(softirq),
-			(unsigned long long)cputime64_to_clock_t(steal),
-			(unsigned long long)cputime64_to_clock_t(guest));
-	}
-	printk("intr %llu", (unsigned long long)sum);
-
-	/* sum again ? it could be updated? */
-	for_each_irq_nr(j) {
-		per_irq_sum = 0;
-		for_each_possible_cpu(i)
-			per_irq_sum += kstat_irqs_cpu(j, i);
-
-		printk(" %u", per_irq_sum);
 	}
 
-	printk(		"\nctxt %llu\n"
-		"btime %lu\n"
-		"processes %lu\n"
-		"procs_running %lu\n"
-		"procs_blocked %lu\n",
-		nr_context_switches(),
-		(unsigned long)jif,
-		total_forks,
-		nr_running(),
-		nr_iowait());
+	ssleep(2);
 
-	printk("softirq %llu", (unsigned long long)sum_softirq);
+	for_each_possible_cpu(i) {
+		user -= cputime64_add(user, kstat_cpu(i).cpustat.user);
+		nice -= cputime64_add(nice, kstat_cpu(i).cpustat.nice);
+		system -= cputime64_add(system, kstat_cpu(i).cpustat.system);
+		idle -= cputime64_add(idle, kstat_cpu(i).cpustat.idle);
+		idle -= cputime64_add(idle, arch_idle_time(i));
+		iowait -= cputime64_add(iowait, kstat_cpu(i).cpustat.iowait);
+		irq -= cputime64_add(irq, kstat_cpu(i).cpustat.irq);
+		softirq -= cputime64_add(softirq, kstat_cpu(i).cpustat.softirq);
+	}
 
-	for (i = 0; i < NR_SOFTIRQS; i++)
-		printk(" %u", per_softirq_sums[i]);
-	printk("\n");
+	printk("TOTAL: %llu\n", total);
+	printk("IDLE: %llu\n", idle);
+	idle *= 100000
+	mod = do_div(idle, total);
+	printk("IDLE: %llu\n", idle);
 
-	return 0;
-}
-asmlinkage long sys_CPU_Utilization(void)
-{
-	show_stat();
 	return 0;
 }
